c47a029fd2fdcc1b2421c52404396a1c84f11b52
東井上　真暉 <>
2024-12-10 14:43:36 +0900

    [Fix] チェリーピック漏れの手直し中

--- Assets/Scripts/Stage/SubManager/EndlessBattleSubManager.cs
+++ Assets/Scripts/Stage/SubManager/EndlessBattleSubManager.cs
@@ -8,17 +8,55 @@ public class EndlessBattleSubManager : StageSubManager
     // ---------- ゲームオブジェクト参照変数宣言 ----------
     // ---------- プレハブ ----------------------------
     // ---------- プロパティ --------------------------
-    [SerializeField, Tooltip("hoge")] private int hoge = default;
+    [SerializeField, Tooltip("hoge")] private List<EndlessBattleSegment> _stagePrefabs = default;
+    [SerializeField, Tooltip("地面")] private Transform _ground = default;
+    private int instanceSegmentIndex = 0;
+    private float nextInstancePos = 0;
+    private Player _player = null;
+    float _nextSegmentPosZ = 0f;
     // ---------- クラス変数宣言 -----------------------
     // ---------- インスタンス変数宣言 ------------------
     // ---------- Unity組込関数 -----------------------
-    private void Start(){
-        
+    protected override void InitializeUnique(){
+        int instanceSegmentIndex = 0;
+        while( _nextSegmentPosZ <= 100f)
+        {
+            EndlessBattleSegment segment = InstantiateSegment();
+            if(instanceSegmentIndex == 1)
+                nextInstancePos = segment.GetLength();
+        }
+        _player = GameDataManager.GetPlayer();
     }
 
-    private void Update(){
+    protected override void UpdateUnique()
+    {
+        if( nextInstancePos <= _player.transform.position.z )
+        {
+            EndlessBattleSegment segment = InstantiateSegment();
+            nextInstancePos += segment.GetLength();
 
+            // 後ろのセグメントを消す
+        }
+        Vector3 pos = _player.transform.position;
+        pos.z += 30f;
+        pos.y = -10f;
+        pos.x = 0f;
+        _ground.transform.position = pos;
     }
     // ---------- Public関数 -------------------------
     // ---------- Private関数 ------------------------
+    private EndlessBattleSegment InstantiateSegment()
+    {
+        EndlessBattleSegment segment = Instantiate(_stagePrefabs[instanceSegmentIndex]);
+        segment.transform.parent = this.transform;
+        segment.transform.localPosition = new Vector3(0, 0, _nextSegmentPosZ);
+        segment.Initialize();
+        _nextSegmentPosZ += segment.GetLength();
+        IndexNext();
+        return segment;
+    }
+    private void IndexNext(){
+        instanceSegmentIndex ++;
+        instanceSegmentIndex %= _stagePrefabs.Count;
+    }
 }
